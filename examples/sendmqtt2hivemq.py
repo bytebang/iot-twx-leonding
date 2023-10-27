6b7c55c56d4a81dc58187f3034b86c192815e9ed
Günther Hutter <guenther.hutter@bytebang.at>
2023-10-27 10:22:13 +0200

    Update sendmqtt2hivemq.py

--- examples/sendmqtt2hivemq.py
+++ examples/sendmqtt2hivemq.py
@@ -83,7 +83,7 @@ client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 # set username and password
 client.username_pw_set("iottest", "IotTest123")
 # connect to HiveMQ Cloud on port 8883 (default for MQTT)
-client.connect("dc5a170db2354a828896bc071195f7dd.s2.eu.hivemq.cloud", 8883)
+client.connect("idofyourbrokerhere.s2.eu.hivemq.cloud", 8883)
 
 # setting callbacks, use separate functions like above for better visibility
 client.on_subscribe = on_subscribe
